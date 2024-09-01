6e4539543bf92e097a51500ed5561ff857cae759
pavithra-n0516 <pavithranakshatriworks@gmail.com>
2024-09-01 16:57:43 +0530

    add all

---
+++ order.php
@@ -0,0 +1,19 @@
+<?php
+  $name = $_POST['name'];
+ $email = $_POST['email'];
+ $password = $_POST['password'];
+ $items = $_POST['items'];
+
+ $conn = new mysqli('localhost','root','','exam');
+  if ($conn->connect_error){
+        die('Connection failed : ' .$conn->connect_error);
+  }else{
+      $stmt = $conn->prepare("insert into order(name, email, password, items)
+                   values(?,?,?,?)");
+$stmt->bind_param("ssss",$name, $email, $password, $items);
+$stmt->execute();
+echo "successfully added to cart...";
+$stmt->close();
+$conn->close();
+}
+?>
